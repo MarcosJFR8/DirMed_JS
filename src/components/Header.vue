46c4396856912dbfca78724dd3b7e786fd923bda
MarcosJFR8 <marcosjfr3@gmail.com>
2025-04-01 10:52:15 -0400

    header arrays closer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,17 +6,17 @@
 
 <template>
     <header class="bg-white shadow-sm">
-        <div class="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div class="container px-4 py-4 justify-between flex items-center">
             <div class="flex items-center">
                 <img src="" alt="que buena shiet" class="h-12">
             </div>
-            <div class="flex items-center space-x-6">
-                <div class="h-8 w-px bg-gray-300">
-                    <nav class="flex space-x-6">
+            <div class="flex items-center">
+                <div class="h-8 w-px bg-gray-300 mx-70">
+                    <nav class="flex space-x-4">
                         <a href="#" class="text-gray-600 hover:text-blue-600">Personal</a>
                         <a href="#" class="text-gray-600 hover:text-blue-600">Contactos</a>
                         <a href="#" class="text-gray-600 hover:text-blue-600">Noticias</a>
-                        <a href="#" class="text-gray-600 hover:text-blue-600">Trabaja con nosotros</a>
+                        <a href="#" class="text-gray-600 hover:text-blue-600 text">Trabajo</a>
                     </nav>
                 </div>
             </div>
